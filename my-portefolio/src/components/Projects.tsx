0c8a6c1777783386bf99f6989f09fde6b223e74c
Rafael Martins <A49444@alunos.isel.pt>
2025-01-18 01:48:33 +0000

    Add project images; TODO: resize

--- my-portefolio/src/components/Projects.tsx
+++ my-portefolio/src/components/Projects.tsx
@@ -3,12 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 
 export function Projects() {
+    const images = {
+        p1: ["projectImages/chimp.jpeg", "projectImages/chimp2.jpeg", "projectImages/chimp3.jpeg"],
+        p2: ["projectImages/games.jpeg", "projectImages/games2.jpeg", "projectImages/games3.jpeg"],
+        p3: ["projectImages/seca.jpeg", "projectImages/seca2.jpeg", "projectImages/seca3.jpeg", "projectImages/seca4.jpeg"],
+        p4: ["projectImages/gomoku.jpeg", "projectImages/gomoku2.jpeg", "projectImages/gomoku3.jpeg"]
+    };
+
+    const [currentImageIndex, setCurrentImageIndex] = React.useState({
+        p1: 0,
+        p2: 0,
+        p3: 0,
+        p4: 0
+    })
+
+    const handleNextImage = (id) => {
+        setCurrentImageIndex((prevIndices) => ({
+            ...prevIndices,
+            [id]: (prevIndices[id] + 1) % images[id].length
+        }));
+    }
+
+    const handlePreviousImage = (id) => {
+        setCurrentImageIndex((prevIndices) => ({
+            ...prevIndices,
+            [id]: (prevIndices[id] - 1 + images[id].length) % images[id].length
+        }));
+    }
+
     return (
         <div className="projects" id="projects">
             <h1>Projects</h1>
             <div className="d-flex overflow-auto">
                 <div className="col-4 project">
-                    <img src="image.png" alt="Instant Messaging App" />
+                    <button onClick={() => handlePreviousImage('p1')} className="arrow-button">{'<'}</button>
+                    <img src={images.p1[currentImageIndex.p1]} className="project-image" />
+                    <button onClick={() => handleNextImage('p1')} className="arrow-button">{'>'}</button>
                     <h2>Instant Messaging Web App</h2>
                     <h6>
                         <i>TypeScript, React, Node.js, Kotlin, Spring, PostgreSQL, Docker</i>
@@ -19,7 +49,9 @@ export function Projects() {
                     </p>
                 </div>
                 <div className="col-4 project">
-                    <img src="image.png" alt="Instant Messaging App" />
+                    <button onClick={() => handlePreviousImage('p2')} className="arrow-button">{'<'}</button>
+                    <img src={images.p2[currentImageIndex.p2]} className="project-image" />
+                    <button onClick={() => handleNextImage('p2')} className="arrow-button">{'>'}</button>
                     <h2>Game Session Management Web App</h2>
                     <h6>
                         <i>JavaScript, Node.js, HTTP4K, Kotlin, PostgreSQL, Docker, Render</i>
@@ -31,7 +63,9 @@ export function Projects() {
                     </p>
                 </div>
                 <div className="col-4 project">
-                    <img src="image.png" alt="Instant Messaging App" />
+                    <button onClick={() => handlePreviousImage('p3')} className="arrow-button">{'<'}</button>
+                    <img src={images.p3[currentImageIndex.p3]} className="project-image" />
+                    <button onClick={() => handleNextImage('p3')} className="arrow-button">{'>'}</button>
                     <h2>Shows & Events Chelas Web App</h2>
                     <h6>
                         <i>JavaScript, Node.js, HTTP4K, Express, ElasticSearch</i>
@@ -43,7 +77,9 @@ export function Projects() {
                     </p>
                 </div>
                 <div className="col-4 project">
-                    <img src="image.png" alt="Instant Messaging App" />
+                    <button onClick={() => handlePreviousImage('p4')} className="arrow-button">{'<'}</button>
+                    <img src={images.p4[currentImageIndex.p4]} className="project-image" />
+                    <button onClick={() => handleNextImage('p4')} className="arrow-button">{'>'}</button>
                     <h2>Gomoku Royale Android App</h2>
                     <h6>
                         <i>Jetpack Compose, Android Kotlin, Firestore</i>
@@ -54,7 +90,7 @@ export function Projects() {
                     </p>
                 </div>
                 <div className="col-4 project">
-                    <img src="image.png" alt="Instant Messaging App" />
+                    <img src='projectImages/checkers.jpeg' className="project-image" />
                     <h2>Checkers App</h2>
                     <h6>
                         <i>Jetpack Compose, Kotlin, mongoDB</i>
